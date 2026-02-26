b8c445decc500d6b813af06ed65a84768af500ea
Aries2311 <aries.aquino1109@gmail.com>
2026-02-26 22:04:04 +0800

    fixed creators 37

--- public/preferences.js
+++ public/preferences.js
@@ -356,14 +356,14 @@
     }
 
     const hasNewFile = !!profile.file;
-const hasNewDataUrl = !!AVATAR_DATA_URL_FOR_UPLOAD;
-const hasExisting = !!EXISTING_AVATAR_URL;
+    const hasNewDataUrl = !!AVATAR_DATA_URL_FOR_UPLOAD;
+    const hasExisting = !!EXISTING_AVATAR_URL;
 
-if (requireAll && !(hasNewFile || hasNewDataUrl) && !hasExisting) {
-  showAvatarRequiredNotice();
-  toast('Please upload a profile picture.', 'error');
-  scrollToAvatarSection();
-  return false;
+    if (requireAll && !(hasNewFile || hasNewDataUrl) && !hasExisting) {
+    showAvatarRequiredNotice();
+     toast('Please upload a profile picture.', 'error');
+     scrollToAvatarSection();
+   return false;
 }
     return true;
   }
